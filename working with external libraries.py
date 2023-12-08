283c446299677f50a3fd178e95bef62cb9faff5a
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-08 18:38:03 +0530

    Update working with external libraries.py

--- working with external libraries.py
+++ working with external libraries.py
@@ -3,3 +3,9 @@
 import requests
 
 # Function to fetch and display information from a public API
+def fetch_data_from_api(api_url):
+    try:
+        # Make a GET request to the API
+        response = requests.get(api_url)
+
+        # Check if the request was successful (status code 200)
