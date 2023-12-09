093dcdf51e3cffba9b2424e0dcc0abe7ec8792b7
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-09 18:24:49 +0530

    Update Date and Time Operations.py

--- Date and Time Operations.py
+++ Date and Time Operations.py
@@ -7,3 +7,10 @@ def display_current_datetime():
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current Date and Time: {formatted_datetime}")
+
+# Function to calculate the future date after a given number of days
+def calculate_future_date(days):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future Date after {days} days: {formatted_future_date}")
