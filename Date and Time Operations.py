80ce7d3a4378286e3c680c03f546f7eade24b505
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-09 18:24:20 +0530

    Update Date and Time Operations.py

--- Date and Time Operations.py
+++ Date and Time Operations.py
@@ -1,3 +1,8 @@
 # Date and Time Operations
 
 from datetime import datetime, timedelta
+# Function to display the current date and time
+def display_current_datetime():
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_datetime}")
