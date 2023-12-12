992dfcd690f7618be910fd938618609e9662fa9e
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-12 22:55:07 +0530

    Update Regular Expressions in Python.py

--- Regular Expressions in Python.py
+++ Regular Expressions in Python.py
@@ -1,5 +1,9 @@
 # Regular Expressions in Python
 
 import re
+
 # Function to search for a pattern in a given text
 def search_pattern(text, pattern):
+  match = re.search(pattern, text)
+    if match:
+        print(f"Pattern found: {match.group()}")
