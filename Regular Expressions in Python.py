f8933b6d28d2ecc65d2d72babadb95663bd1c6df
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-12 22:56:20 +0530

    Update Regular Expressions in Python.py

--- Regular Expressions in Python.py
+++ Regular Expressions in Python.py
@@ -13,3 +13,6 @@ def search_pattern(text, pattern):
 # Example: Searching for an email address in a text
 text_sample = "Contact us at support@example.com for assistance."
 email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+# Search for the email pattern in the text
+search_pattern(text_sample, email_pattern)
