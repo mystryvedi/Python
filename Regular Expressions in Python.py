9b3e4a3b2447e0fd24fae25ba40a00b11661c6c7
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-12 22:55:59 +0530

    Update Regular Expressions in Python.py

--- Regular Expressions in Python.py
+++ Regular Expressions in Python.py
@@ -9,3 +9,7 @@ def search_pattern(text, pattern):
         print(f"Pattern found: {match.group()}")
     else:
         print("Pattern not found.")
+
+# Example: Searching for an email address in a text
+text_sample = "Contact us at support@example.com for assistance."
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
