2b5f3caf044c54440d6933a063d5cc307ce63cb1
Gyapti Trivedi <115182741+mystryvedi@users.noreply.github.com>
2023-12-27 17:26:20 +0530

    Update JSON Handling in Python.py

--- JSON Handling in Python.py
+++ JSON Handling in Python.py
@@ -1,3 +1,8 @@
 # JSON Handling in Python
 
 import json
+
+# Function to encode (serialize) Python data to JSON format
+def encode_to_json(data):
+    json_data = json.dumps(data, indent=2)
+    return json_data
